4c8bd518e7ef5b47de51deca17d8fc1ff9059b4a
Lorre Huggan <lorre.huggan@protonmail.com>
2025-11-07 14:00:02 +0000

    Refactor query functionality and provider structure

--- lib/utils/appProviders.tsx
+++ lib/utils/appProviders.tsx
@@ -2,20 +2,23 @@ import { useColorScheme } from "react-native";
 
 import { ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { AuthProvider } from "@/features/auth/lib/Provider";
-import { QueryProvider } from "@/features/query/lib/Provider";
+import { QueryProvider } from "@/lib/query/provider";
 import { NAV_THEME } from "@/styles/theme";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const colorScheme = useColorScheme(); // "light" | "dark"
 
   return (
-    <ThemeProvider value={NAV_THEME[colorScheme ?? "light"]}>
-      <QueryProvider>
-        <AuthProvider>{children}</AuthProvider>
-      </QueryProvider>
-      <PortalHost />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider value={NAV_THEME[colorScheme ?? "light"]}>
+        <QueryProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </QueryProvider>
+        <PortalHost />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
